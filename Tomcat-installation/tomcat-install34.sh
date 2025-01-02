9dc755552e835da7da374c8689ecdf7f8688b5ac
Jasgida <160689182+Jasgida@users.noreply.github.com>
2025-01-02 13:30:42 +0100

    Update tomcat-install34.sh

--- Tomcat-installation/tomcat-install34.sh
+++ Tomcat-installation/tomcat-install34.sh
@@ -9,21 +9,21 @@ sudo yum install java-1.8.0-openjdk-devel -y
 # install wget unzip packages.
 sudo yum install wget unzip -y
 
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.97/bin/apache-tomcat-9.0.97.zip
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-11/v11.0.2/bin/apache-tomcat-11.0.2.zip
 
-sudo unzip apache-tomcat-9.0.97.zip
-sudo rm -rf apache-tomcat-9.0.97.zip
+sudo unzip apache-tomcat-11.0.2.zip
+sudo rm -rf apache-tomcat-11.0.2.zip
 ### rename tomcat for good naming convention
-sudo mv apache-tomcat-9.0.97 tomcat9 
+sudo mv apache-tomcat-11.0.2. tomcat11 
 ### assign executable permissions to the tomcat home directory
-sudo chmod 775 -R /opt/tomcat9
-sudo chown ec2-user -R /opt/tomcat9
+sudo chmod 775 -R /opt/tomcat11
+sudo chown ec2-user -R /opt/tomcat11
 ### start tomcat
-sh /opt/tomcat9/bin/startup.sh
+sh /opt/tomcat11/bin/startup.sh
 #sh startup.sh = file not found
 # create a soft link to start and stop tomcat from anywhere
 # This will enable us to manage tomcat as a service
-sudo ln -s /opt/tomcat9/bin/startup.sh /usr/bin/starttomcat
-sudo ln -s /opt/tomcat9/bin/shutdown.sh /usr/bin/stoptomcat
+sudo ln -s /opt/tomcat11/bin/startup.sh /usr/bin/starttomcat
+sudo ln -s /opt/tomcat11/bin/shutdown.sh /usr/bin/stoptomcat
 starttomcat
 sudo su - ec2-user
